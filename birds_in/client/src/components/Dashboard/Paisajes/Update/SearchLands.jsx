fe9f4ffce5c94e1ddbf0c72d31fadbcd2cbc1f30
ilcjo <ileanacanoic@gmail.com>
2024-10-08 00:43:51 -0500

    search land por zona y pais

--- birds_in/client/src/components/Dashboard/Paisajes/Update/SearchLands.jsx
+++ birds_in/client/src/components/Dashboard/Paisajes/Update/SearchLands.jsx
@@ -22,7 +22,7 @@ export const SearchLands = ({ changeTab }) => {
     const [loadingMessage, setLoadingMessage] = React.useState('Cargando...');
     const [selectedRegister, setSelectedRegister] = React.useState(null);
     const [registerData, setRegisterData] = React.useState([]);
-    // console.log('registro seleccionado', registerData)
+    console.log('registro seleccionado', selectedRegister)
     const [showUpdateRegister, setShowUpdateRegister] = React.useState(false);
     const [showSearchRegister, setShowSearchRegister] = React.useState(true);
 
@@ -49,26 +49,63 @@ export const SearchLands = ({ changeTab }) => {
     }, [selectedRegister]);
 
     //encuentra y organiza los registros de la db
+    // React.useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             setShowBackdrop(true)
+    //             const response = await axios.get('/paisajes/filtros?page=0&perPage=0');
+    //             const data = response.data.RegistrosFiltrados;
+    //             const validData = data.filter((item) => item.zona);
+    //             // Ordenar los datos válidos por nombre de zona
+    //             validData.sort((a, b) => a.zona.nombre.localeCompare(b.zona.nombre));
+    //             localStorage.setItem('LandsData', JSON.stringify(data));
+    //             setRegisterData(validData);
+    //         } catch (error) {
+    //             console.error("Error al obtener los datos:", error);
+    //         } finally {
+    //             setShowBackdrop(false);
+    //         }
+    //     };
+
+    //     fetchData();
+    // }, [showUpdateRegister]);
+
     React.useEffect(() => {
         const fetchData = async () => {
             try {
-                setShowBackdrop(true)
+                setShowBackdrop(true);
                 const response = await axios.get('/paisajes/filtros?page=0&perPage=0');
                 const data = response.data.RegistrosFiltrados;
-                const validData = data.filter((item) => item.zona);
-                // Ordenar los datos válidos por nombre de zona
-                validData.sort((a, b) => a.zona.nombre.localeCompare(b.zona.nombre));
-                localStorage.setItem('LandsData', JSON.stringify(data));
-                setRegisterData(validData);
+                console.log(data);
+    
+                // Filtrar los registros para que incluyan solo aquellos con una zona o un país
+                const validData = data.filter((item) => item.zona || item.paise); // Asegúrate de que el nombre de la propiedad sea correcto
+                console.log(validData);
+    
+                // Mapear los datos para mostrar el nombre de la zona o el país
+                const formattedData = validData.map((item) => {
+                    // Verifica si hay zona y usa el nombre correspondiente
+                    const nombre = item.zona ? item.zona.nombre : item.paise.nombre;
+                    const id = item.id
+    
+                    return {
+                        id: id,
+                        nombre: nombre,
+                        // Incluye otras propiedades necesarias aquí
+                    };
+                });
+    
+                localStorage.setItem('LandsData', JSON.stringify(formattedData)); // Guarda los datos formateados en localStorage
+                setRegisterData(formattedData); // Actualiza el estado con los datos formateados
             } catch (error) {
                 console.error("Error al obtener los datos:", error);
             } finally {
                 setShowBackdrop(false);
             }
         };
-
         fetchData();
     }, [showUpdateRegister]);
+    
 
 
     return (
@@ -99,7 +136,7 @@ export const SearchLands = ({ changeTab }) => {
                             <Autocomplete
                                 id="search_Paisaje"
                                 options={registerData}
-                                getOptionLabel={(option) => option.zona.nombre}
+                                getOptionLabel={(option) => option.nombre}
                                 // filterOptions={(options, state) => {
                                 //     const inputValue = state.inputValue.toLowerCase().trim(); // Convertir a minúsculas y quitar espacios en blanco
                                 //     return options.filter((option) => {
